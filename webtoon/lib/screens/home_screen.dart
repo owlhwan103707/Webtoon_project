a6fe3fc885069a07a2f57359846536bbb62a300b
owlhwan103707 <ojihwan@ojihwan-ui-MacBookAir.local>
2024-07-09 22:24:42 +0900

    FutureBuilder사용

--- webtoon/lib/screens/home_screen.dart
+++ webtoon/lib/screens/home_screen.dart
@@ -3,39 +3,15 @@ import 'package:webtoon/services/api_service.dart';
 
 import '../models/webtoon_model.dart';
 
-class Homescreen extends StatefulWidget{
-  const Homescreen ({super.key});
+class Homescreen extends StatelessWidget{
+  Homescreen ({super.key});
 
-  @override
-  State<Homescreen> createState() => _HomescreenState();
-}
-
-class _HomescreenState extends State<Homescreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async{ //HTTP 요청을 기다리는 함수
-    webtoons = await ApiService.getTodaysToons(); //
-    isLoading = false; //값을 false로
-    setState(() {//화면 새로고침
-
-    });
-
-  }
-
-
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();//initstate에서 데이터를 받아오는 함수를 호출하고 받아오기가 끝나면 webtoons에 넣는다.
-  }
+  Future <List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context)
   {
-    print(webtoons);
-    print(isLoading);
+
     return Scaffold(//screen을 위한 기본적인 레이아웃과 설정을 제공함
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -46,6 +22,17 @@ class _HomescreenState extends State<Homescreen> {
         title: const Text("오늘의 웹툰",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w400),),
       ),
 
+
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot){
+          if(snapshot.hasData)
+            {
+              return const Text("There is data");
+            }
+          return const Text("로딩");
+        },
+      ),
     );
   }
 }
